40b231347be3551b8d3fef2116e57883e57a937a
Roland Haas <rhaas@ncsa.illinois.edu>
2017-10-29 12:38:41 -0500

    async_writer: expand test to test finalize() and select() calls

--- async_writer/test.cc
+++ async_writer/test.cc
@@ -3,15 +3,21 @@
 
 #include <cstdio>
 #include <cstring>
+#include <unistd.h>
 
 int main(void)
 {
   {
     async_writer writer(stdout);
     writer.write(strdup("foo\n"), 4);
+    fprintf(stderr, "available: %zu\n", writer.select(100));
+    sleep(1);
+    fprintf(stderr, "available: %zu\n", writer.select(100));
     writer.seek(1);
     writer.write(strdup("aa"), 2);
+    fprintf(stderr, "available: %zu\n", writer.select(100));
     writer.finalize();
+    fprintf(stderr, "available: %zu\n", writer.select(100));
   }
   return 0;
 }
